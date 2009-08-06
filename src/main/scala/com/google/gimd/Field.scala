c71f4dba231216109a3a39412b97296c21b00797
Grzegorz Kossakowski <grek@google.com>
2009-08-06 15:32:38 -0700

    Define projections for each Field type.
    The idea is that abstract type can project itself to one of concrete
    subtypes or throw an exception. This is useful when one has a collection
    of fields and wants to project them into collection of specific field.
    
    Signed-off-by: Grzegorz Kossakowski <grek@google.com>

--- src/main/scala/com/google/gimd/Field.scala
+++ src/main/scala/com/google/gimd/Field.scala
@@ -61,29 +61,64 @@ abstract sealed case class Field() extends Ordered[Field] with Comparable[Field]
     val scale = a.scale max b.scale
     a.setScale(scale) compare b.setScale(scale)
   }
+
+  //projections
+  def stringField: StringField =
+    throw new TypeMismatchError(classOf[StringField], this)
+  def timestampField: TimestampField =
+    throw new TypeMismatchError(classOf[TimestampField], this)
+  def messageField: MessageField =
+    throw new TypeMismatchError(classOf[MessageField], this)
+  def intField: IntField =
+    throw new TypeMismatchError(classOf[IntField], this)
+  def longField: LongField =
+    throw new TypeMismatchError(classOf[LongField], this)
+  def bigIntField: BigIntField =
+    throw new TypeMismatchError(classOf[BigIntField], this)
+  def bigDecimalField: BigDecimalField =
+    throw new TypeMismatchError(classOf[BigDecimalField], this)
+
 }
 
-sealed case class StringField(name: String, value: String) extends Field
-sealed case class TimestampField(name: String, value: Timestamp) extends Field
-sealed case class MessageField(name: String, value: Message) extends Field
+sealed case class StringField(name: String, value: String) extends Field {
+  override def stringField = this
+}
+sealed case class TimestampField(name: String, value: Timestamp) extends Field {
+  override def timestampField = this
+  override def stringField = StringField(name, Formatter.valueString(this))
+}
+sealed case class MessageField(name: String, value: Message) extends Field {
+  override def messageField = this
+}
 
 abstract sealed case class NumberField() extends Field {
   def toNumber: Number
   def toBigDecimal: BigDecimal
+  override def stringField = StringField(name, Formatter.valueString(this))
 }
 sealed case class IntField(name: String, value: Int) extends NumberField {
   def toNumber = value
   def toBigDecimal = BigDecimal(value)
+  override def intField = this
+  override def longField = LongField(name, value)
+  override def bigIntField = BigIntField(name, value)
+  override def bigDecimalField = BigDecimalField(name, toBigDecimal)
 }
 sealed case class LongField(name: String, value: Long) extends NumberField {
   def toNumber = value
   def toBigDecimal = BigDecimal(value)
+  override def longField = this
+  override def bigIntField = BigIntField(name, value)
+  override def bigDecimalField = BigDecimalField(name, toBigDecimal)
 }
 sealed case class BigIntField(name: String, value: BigInt) extends NumberField {
   def toNumber = value
   def toBigDecimal = BigDecimal(value)
+  override def bigIntField = this
+  override def bigDecimalField = BigDecimalField(name, toBigDecimal)
 }
 sealed case class BigDecimalField(name: String, value: BigDecimal) extends NumberField {
   def toNumber = value
   def toBigDecimal = value
+  override def bigDecimalField = this
 }
