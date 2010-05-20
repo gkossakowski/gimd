bfc0300fa49310d2e692eef14a9b673fee64a1da
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2010-05-21 01:27:08 +0200

    Added IntelliJ plug-in to SBT's project definition.
    This plug-in allows sbt to generate project files automatically.
    
    Updated .gitignore rules accordingly.
    
    Change-Id: I481f9140144fcc3c85f6ea6c7434ce25b639b52c
    Signed-off-by: Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -9,14 +9,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import sbt._
-
-class GimdProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
-  val jgitRepo = "jgit-repository" at "http://egit.googlecode.com/svn/maven/"
-  val scalaTools = "scala-tools" at "http://scala-tools.org/repo-releases"
-  val junit = "junit" % "junit" % "4.6"
-  val jgit = "org.spearce" % "jgit" % "0.5.0-93-g5b89a2c"
-  val commonsIo = "commons-io" % "commons-io" % "1.3.2"
-  val scalaCheck = "org.scala-tools.testing" % "scalacheck_2.8.0.RC1" % "1.7"
+class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val repo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
+  val idea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
 }
-
